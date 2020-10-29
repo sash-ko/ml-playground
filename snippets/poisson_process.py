4aa69bcbfb28d4498836c9ec014a141b2b953ad7
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-10-29 20:52:40 +0100

    Update poisson_process.py

--- snippets/poisson_process.py
+++ snippets/poisson_process.py
@@ -1,19 +1,21 @@
 import numpy as np
 
 
-def poisson_process(time_period, num_events):
+def poisson_process(time_period: int, num_events: int) -> np.array:
     """ Return a list of time points when events happened
     during time_period, e.g. [0.12, 1.23, 1.56, 2.33, 4.76]
     
     NOTE: the number of time points many not be equal to num_events
     """
+    
+    # average number of events per time period
     lambda_ = num_events / time_period
 
     events = np.random.exponential(1.0 / lambda_, num_events)
     return np.cumsum(events)
 
 
-def discrete_poisson_process(duration, num_events):
+def discrete_poisson_process(duration: int, num_events: int) -> np.array:
     """ Poisson process to generate a list of event counts for each discrete
     step of "duration":
 
@@ -21,6 +23,7 @@ def discrete_poisson_process(duration, num_events):
     >> [0 1 1 3 1 2 0 0 0 0]
     """
 
+    # average number of events per time period
     lambda_ = num_events / duration
 
     events = np.random.exponential(1.0 / lambda_, num_events)
